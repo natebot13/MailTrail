37963d9352629525f6e3a54d8d13c7a4a3684491
Nathan Pannell <tekkbot@gmail.com>
2016-02-13 19:06:12 -0800

    server temp

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 """
 This script runs the FlaskWebProject application using a development server.
 """
+import sys
+sys.path.insert(0, '/var/www/MailTrail')
 from os import environ
 from views import app
 
