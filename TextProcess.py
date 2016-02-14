3a426e10c710d933760d1f10409e0a66357d60fd
Marc Davis <marc.davis@berkeley.edu>
2016-02-14 07:38:45 -0800

    removed credentials

--- TextProcess.py
+++ TextProcess.py
@@ -3,13 +3,10 @@ import Gameplay
 import twilio.twiml
 from twilio.rest import TwilioRestClient
 
+from NotOnGit import client, sg
+
 import sendgrid
 
-account_sid = "AC3930798939ffc71eddac1cf3e515a462"
-auth_token = "6a08e5998c52b12de9b4b36728ff2ad8"
-client = TwilioRestClient(account_sid, auth_token)
-
-sg = sendgrid.SendGridClient('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')
 
 email_url = '@mailtrailgame.com'
 
