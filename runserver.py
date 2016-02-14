570fbe29425c3a82561735d439825f5a367bc3bc
Nathan Pannell <tekkbot@gmail.com>
2016-02-13 19:04:46 -0800

    hopefully it works finally

--- runserver.py
+++ runserver.py
@@ -2,7 +2,7 @@
 This script runs the FlaskWebProject application using a development server.
 """
 from os import environ
-from views import app
+from views import app as application
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    application.run(HOST, PORT)
---
+++ runserver.wsgi
@@ -0,0 +1,16 @@
+"""
+This script runs the FlaskWebProject application using a development server.
+"""
+import sys
+ysy.path.insert(0, '/vaw/www/MailTrail')
+
+from os import environ
+from views import app as application
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    application.run(HOST, PORT)
