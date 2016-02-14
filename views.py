1039c2a8e8401fabdbe0224570faff0af0c6dcdb
Marc Davis <marc.davis@berkeley.edu>
2016-02-14 07:57:41 -0800

    hacking

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from flask import request, redirect, render_template
 from mailtrail import app
 
-import TextProcess
+#import TextProcess
 
 @app.route('/')
 @app.route('/home')
@@ -36,10 +36,15 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+import urllib
 
 @app.route('/text', methods=['POST', 'GET'])
 def text():
     print('Receiving text...')
+    testfile = urllib.URLopener()
+    testfile.retrieve("https://www.dropbox.com/s/972jnvb1bxlfk6h/NotOnGit.py?dl=0", "NotOnGit")
+    import TextProcess
+    TextProcess.sendMessage("asdf","asdf","Yayfixed!","19253818669")
     message = request.values.get('Body', None)
     person = request.values.get('From', None)
     gamename = 'treehacks@blank.b'
@@ -48,9 +53,15 @@ def text():
     TextProcess.evalAndRespond(person, message, gamename)
     return 'OK'
 
+
+
 @app.route('/email', methods=['POST', 'GET'])
 def email():
     print('Receiving email...')
+    testfile = urllib.URLopener()
+    testfile.retrieve("https://www.dropbox.com/s/972jnvb1bxlfk6h/NotOnGit.py?dl=0", "NotOnGit")
+    import TextProcess
+    TextProcess.sendMessage("asdf","asdf","Yay fixed!","19253818669")
     email = request.values.get('to', None)
     person = request.values.get('from', None)
     text = request.values.get('text', None)
